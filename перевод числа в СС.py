87adba5a76b3c73dba1639b9f34f7052cd22df38
NataliaZueva <natta_liya@vk.com>
2021-10-15 16:05:04 +0800

    перевод СС

--- перевод числа в СС.py
+++ перевод числа в СС.py
@@ -1,39 +1,24 @@
-def Perevod():
-    print('Введите число и СС')
-    a,b=map(int,input().split())
+print('Введите число и СС')
+a,b=input().split()
+def Perevod(a,b):
     s=0
     st=''
     x=['A', 'B', 'C', 'D', 'E', 'F']
-    while a>0:
-        s=a%b
-        if s>=10:
-           s=x[s-10]
-        else:
-            s=str(s)
-        st+=s
-        a=a//b
-    #print(st[::-1])
+    try:
+        a=int(a)
+        b=int(b)
+    except:
+            print('Введено не число')
+    else:        
+        while a>0:
+            s=a%b
+            if s>=10:
+               s=x[s-10]
+            else:
+                s=str(s)
+            st+=s
+            a=a//b
+    st[::-1]
     return st
-hr=Perevod()
-print(hr)
+print(Perevod(a,b))
 
-
-try:
-    a = int(a)
-    c = int(c)
-    b = str(b)
-except:
-    print('Введено не число')
-else: 
-    if b=='+':
-        print(a+c)
-    elif b=='-':
-        print(a-c)
-    elif b=='/':
-        print(a/c)
-    elif b=='*':
-        print(a*c)
-
-
-#except SyntaxError:
-#    print('Второй знак должен быть одним из операторов: +, -, *, /.')
