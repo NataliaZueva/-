a4cff21824e7331675d2d4d5a27749ac7b419ee3
NataliaZueva <91422734+NataliaZueva@users.noreply.github.com>
2022-03-26 13:12:41 +0800

    Изменение

--- 1.2/03.18_.py
+++ 1.2/03.18_.py
@@ -1,6 +1,9 @@
-a = list(map(int, input('Введите числа: ').split()))
-k=0
+a, k = list(map(int, input('Введите числа: ').split())), 0
 for i in range (len(a)):
     if (a[i] % 2)  == 0:
         k+=1
 print(k)
+
+print(len(list(filter(lambda x: (x % 2 == 0), list(map(int,input().split()))))))
+
+
---
+++ 1.2/03.25_Hexagon.py
@@ -0,0 +1,24 @@
+import math
+class Hexagon:
+    def __init__(self, r):
+        self.__r = r
+        self.__area = (3 * math.sqrt(3)* self.__r**2)/2
+
+    def set_r(self, new_r):
+        self.__r = new_r
+        self.calc_area()
+
+    def r(self):
+        return self.__r
+
+    def area(self):
+        return self.__area
+
+    def calc_area(self):
+        self.__area = (3 * sqrt(3)* self.__r^2)/2
+
+    def __str__ (self):
+        return str(self.__r) 
+
+q = Hexagon(int(input("Введите радиус: ")))
+print("area", q.area())
