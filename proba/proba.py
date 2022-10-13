d1e41794bb3eb08e7733ab9f7a6a6c7fbae5678c
NataliaZueva <91422734+NataliaZueva@users.noreply.github.com>
2022-10-13 12:36:39 +0800

    Sun sistem - completion program

--- proba/proba.py
+++ proba/proba.py
@@ -1,38 +1,16 @@
-# def file_read(title):
-#     a = []
-#     b = open(title, 'r').readlines()
-#     for line in b:
-#         a.append([x for x in line.split()])
-#     return a
-#
-#
-# pla = file_read('Planes.txt')
-# print(pla)
-#
-#
-#
-# class MyClass:
-#     def __init__(self, a):
-#         self.a = a
-#         print("Hello", a)
-#
-#
-# other_object = {}
-# objs = [MyClass(pla[i]) for i in range(8)]
-# for obj in objs:
-#     other_object.add(obj)
-#
-# objs[0].do_sth()
-#
-#
-#
-#
-# # i = 0
-# # while i <= 7:
-# #     exec("x{} = {}".format(i, i))
-# #     i += 1
-# #
-# # print(x4)
+def file_read(title):
+    a = []
+    b = open(title, 'r').readlines()
+    for line in b:
+        a.append([x for x in line.split()])
+    return a
 
 
+tx = file_read("name.txt")
+txt = []
+pos = []
+for i in tx:
+    txt.append([str(i[0]), bool(i[1]), (int(i[2]), int(i[3]), int(i[4]))])
+    pos.append((int(i[5]), int(i[6])))
 
+print(txt)
