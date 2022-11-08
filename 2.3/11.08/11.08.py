f398e4956c859c1accbce4c8c183de1c0f94b7d6
NataliaZueva <91422734+NataliaZueva@users.noreply.github.com>
2022-11-08 11:01:11 +0800

    + 08.11

--- 2.3/11.08/11.08.py
+++ 2.3/11.08/11.08.py
@@ -1,9 +1,23 @@
 import time
+
 # задача 1
-a, b, c = input().split()
-startTime = time.time()
-a, b, c = b, c, a
-endTime = time.time()
-print(a, b, c)
-totalTime = endTime
-print("Время, затраченное на выполнение = ", totalTime)
+# a, b, c = input().split()
+# startTime = time.time()
+# a, b, c = b, c, a
+# endTime = time.time()
+# print(a, b, c)
+# totalTime = endTime
+# print("Время, затраченное на выполнение = ", totalTime)
+
+# задача 2
+flag = True
+while flag:
+    a, b = input("Введите 2 числа: ").split()
+    try:
+        a = int(a)
+        b = int(b)
+    except ValueError:
+        print('Введено не число')
+    else:
+        print(a + b)
+        flag = False
