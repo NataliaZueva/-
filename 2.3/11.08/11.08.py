78ff31509daad44ded36d0538543af8253de8c55
NataliaZueva <91422734+NataliaZueva@users.noreply.github.com>
2022-11-08 11:25:09 +0800

    + 08.11

--- 2.3/11.08/11.08.py
+++ 2.3/11.08/11.08.py
@@ -27,13 +27,35 @@ from random import randint
 # задача 2.2
 
 # # задача 3.1
-x = randint(0, 100)
+# x = randint(0, 100)
 # x1 = x ** 5
 # print(x1)
 
 
-# задача 3.2
-x2 = 1
-for i in range(5):
-    x2 = x * x2
-print(x2)
+# # задача 3.2
+# x2 = 1
+# for i in range(5):
+#     x2 = x * x2
+# print(x2)
+
+# задача 4
+i = 0
+a = 1
+f = []
+while i <= 250:
+    i += a
+    a = i - a
+    f.append(i)
+flag = True
+while flag:
+    c = int(input("Введите число от 0 до 250: "))
+    if (c >= 0) and (c <= 250):
+        flag = False
+    else:
+        print("Кажется вы не попали в промежуток чисел...")
+if c in f:
+    print("Это число входит в список фибоначи")
+else:
+    print("Это число не входит в список фибоначи")
+
+
